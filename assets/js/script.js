c08604bae3fe2f2bf6d506ac9be614d3dcc4b89a
Stiouf <stephane.quentin@live.fr>
2018-10-05 14:08:27 +0200

    ex1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,2 @@
-var a = "La Manu";
-alert("Bienvenue à " + a);
+var a = "La Manu"; // déclaration d'une variable a avec La Manu à l'intérieur
+alert("Bienvenue à " + a); //affichage du contenu de la varaible a avec du texte entre ""
